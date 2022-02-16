0cad64fd552783b8657b659ba902993886d5d0d3
RadiaktiveLive <98389220+RadiaktiveLive@users.noreply.github.com>
2022-02-16 13:40:50 +0100

    Fase 2 terminada por Xavi ;)

--- src/Main.java
+++ src/Main.java
@@ -5,48 +5,75 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
         List<Question> questions = buildQuestionList();
-        int counter = askTheQuestions(questions);
-        System.out.println("El número total de respuestas correctas es: " + counter + ". Numero total de preguntas: " + questions.size());
+
+        askTheQuestions(questions);
+        int correctAnswers = getCorrectAnswers(questions);
+        int totalScore = getTotalScore(questions);
+        printAnswers(questions);
+
+        System.out.println("Has acertado un total de " + correctAnswers + " sobre " + questions.size() + " preguntas");
+        System.out.println("Has obtenido un total de " + totalScore + " puntos");
     }
 
-    private static int askTheQuestions(List<Question> questions) {
+    private static void printAnswers(List<Question> questions) {
+        for (Question currentQuestion : questions) {
+            System.out.println("Para la pregunta: " + currentQuestion.getStatement());
+            System.out.println("La respuesta era: " + currentQuestion.isCorrectAnswer());
+            System.out.println("Y tu has responidido: " + currentQuestion.isUserAnswer());
+            System.out.println("");
+        }
+    }
 
-        int counterRightAnwsers = 0;
+    private static int getTotalScore(List<Question> questions) {
+        int totalScore = 0;
+        for (Question currentQuestion : questions) {
+            if (currentQuestion.isAnswerCorrect()) {
+                totalScore += currentQuestion.getDifficulty();
+            }
+        }
+        return totalScore;
+    }
 
+
+    private static int getCorrectAnswers(List<Question> questions) {
+        int correctAnswers = 0;
+        for (Question currentQuestion : questions) {
+            if (currentQuestion.isCorrectAnswer()) {
+                correctAnswers++;
+            }
+        }
+        return correctAnswers;
+    }
+
+    private static void askTheQuestions(List<Question> questions) {
         for (int i = 0; i < questions.size(); i++) {
-
             Question currentQuestion = questions.get(i);
 
             System.out.println(currentQuestion.getStatement());
-
             System.out.println("Contesta True/False (T/F)");
             Scanner sc = new Scanner(System.in);
             String text = sc.nextLine();
 
             boolean userTrueOrFalse = text.equalsIgnoreCase("T");
+            currentQuestion.setUserAnswer(userTrueOrFalse);
 
-            if (userTrueOrFalse == currentQuestion.isTrueOrFalse()) {
-                counterRightAnwsers++;
+            if (currentQuestion.isAnswerCorrect()) {
                 System.out.println("Has acertado!!!");
-            }
-            else {
+            } else {
                 System.out.println("Nice try, pero no chaval ;-)");
             }
         }
-
-        return counterRightAnwsers;
     }
 
     private static List<Question> buildQuestionList() {
         List<Question> questions = new ArrayList<>();
 
-        questions.add(new Question("La capital de Francia es Paris", true));
-        questions.add(new Question("La capital de Italia es Roma", true));
-        questions.add(new Question("La capital de Portugal es Lisboa", true));
-        questions.add(new Question("La capital de Alemania es Londres", false));
-        questions.add(new Question("La capital de Holanda es Bruselas", false));
+        questions.add(new Question("La capital de Francia es Paris", true, 3));
+        questions.add(new Question("La capital de Italia es Roma", true, 2));
+        questions.add(new Question("La capital de Portugal es Lisboa", true, 4));
+        questions.add(new Question("La capital de Alemania es Londres", false, 5));
+        questions.add(new Question("La capital de Holanda es Bruselas", false, 5));
 
         return questions;
     }
--- src/Question.java
+++ src/Question.java
@@ -1,18 +1,37 @@
 public class Question {
     private String statement;
-    private boolean trueOrFalse;
+    private boolean correctAnswer;
+    private boolean userAnswer;
+    private int difficulty;
 
-    public Question(String statement, boolean trueOrFalse) {
+    public Question(String statement, boolean correctAnswer, int difficulty) {
         this.statement = statement;
-        this.trueOrFalse = trueOrFalse;
+        this.correctAnswer = correctAnswer;
+        this.userAnswer = false;
+        this.difficulty = difficulty;
     }
 
     public String getStatement() {
         return statement;
     }
 
-    public boolean isTrueOrFalse() {
-        return trueOrFalse;
+    public boolean isCorrectAnswer() {
+        return correctAnswer;
+    }
 
+    public void setUserAnswer(boolean userAnswer) {
+        this.userAnswer = userAnswer;
+    }
+
+    public boolean isUserAnswer() {
+        return userAnswer;
+    }
+
+    public boolean isAnswerCorrect(){
+        return correctAnswer == userAnswer;
+    }
+
+    public int getDifficulty() {
+        return difficulty;
     }
 }
